6044df3eead776549f73e04e18a360bbecaddc3f
nihalgopalam <62307627+nihalgopalam@users.noreply.github.com>
2024-05-21 08:44:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,35 @@ from PIL import Image
 import random
 import rsa
 import math
+import os
 
-
-# to create a new 10x10 image
-def newImage(width, length):
+# generate a new base image to encrypt
+def imageGeneration(width, length):
     image = Image.new("RGB", (length,width))
     x = image.getdata()
     image1 = []
+    # get random rgb values for each pixel
     for i in x:
-        image1.append(((int(random.random()*256)),(int(random.random()*256)),(int(random.random()*256))))
-
+        image1.append((random.randint(0,256)),(random.randint(0,256)),(random.randint(0,256)))
     image.putdata(image1)
-    filename = "image{width}{length}.jpg"
+    filename = "image.jpg"
     image.save(filename)
     return filename
 
+# generate public and private rsa keys and save to file
+def keyGeneration():    
+    publicKeyFile = open('publicKeyRSA', 'w+b')
+    privateKeyFile = open('privateKeyRSA', 'w+b')
+    (pub, priv) = rsa.newkeys(512)
+    publicKeyFile.write(pub.save_pkcs1())
+    privateKeyFile.write(priv.save_pkcs1())
+    return publicKeyFile, privateKeyFile
 
-(pub, priv) = rsa.newkeys(512)
-
+# get the keys from files
+public = open('publicKeyRSA', 'rb')
+private = open('privateKeyRSA', 'rb')
+publicKey = rsa.PublicKey.load_pkcs1(public.read())
+privateKey = rsa.PrivateKey.load_pkcs1(private.read())
 
 image = Image.open("image.jpg")
 data = image.getdata()
@@ -27,34 +38,40 @@ data = image.getdata()
 original = []
 cipher = []
 message = []
+cipherImage = []
 
-cImage = []
-
+# todo: fix trucatating values when spliting
 for i in data:
     original.append(i[0])
     x = (str(i[0])).encode()
-    encrypt = rsa.encrypt(x, pub)
+    encrypt = rsa.encrypt(x, publicKey)
     cipher.append(encrypt)
     val = str(int.from_bytes(encrypt, "big"))
-    cImage.append((((1/int(val[:51]))*256), ((1/int(val[51:102]))*256), ((1/int(val[102:]))*256)))
+    cipherImage.append((((1/int(val[:51]))*256), ((1/int(val[51:102]))*256), ((1/int(val[102:]))*256)))
     # print((str(1/int(val[:51])*256), str(1/int(val[51:102])*256), str(1/int(val[102:])*256)))
 
 
     
 
 for i in cipher:
-    decrypt = rsa.decrypt(i, priv)
+    decrypt = rsa.decrypt(i, privateKey)
     message.append(decrypt.decode())
 
 
 print(original)
-print(cImage)
+print(cipherImage)
 
 image1 = Image.new("RGB", (10,10))
-image1.putdata(cImage)
+image1.putdata(cipherImage)
 image1.show()
 
 # print(string)
+
+
+
+
+
+
 
 
 
